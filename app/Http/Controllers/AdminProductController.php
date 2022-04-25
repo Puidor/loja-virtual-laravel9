baa6d0eb1980654ebce9a70047b3f3799c2f0a3e
Puidor <c.adriano.silva@hotmail.com>
2022-04-25 09:32:34 -0300

    Admin Create Product DB

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Str;
 
 class AdminProductController extends Controller
 {
@@ -32,8 +33,34 @@ class AdminProductController extends Controller
     }
 
     //Recebe a requisição do formulário de criação (POST)
-    public function store()
+    public function store(Request $request)
     {
+        $input = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'string|nullable',
+            'price' => 'string|required',
+            'stock' => 'integer|nullable',
+            // 'cover' => 'file|nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'cover' => 'file|nullable',
+        ], [
+            'name.required' => 'O nome do produto é obrigatório',
+            'name.max' => 'O nome do produto deve ter no máximo 255 caracteres',
+            'price.required' => 'O preço do produto é obrigatório',
+            'stock.integer' => 'O estoque do produto deve ser um número',
+            'cover.file' => 'O arquivo deve ser uma imagem',
+            'cover.max' => 'A imagem deve ter no máximo 2MB',
+        ]);
 
+        $input['slug'] = Str::slug($input['name']);
+
+        if(!empty($input['cover']) && $input['cover']->isValid()){
+            $file = $input['cover'];
+            $path = $file->store('public');
+            $input['cover'] = $path;
+        }
+
+        Product::create($input);
+
+        return redirect()->route('admin.products');
     }
 }
